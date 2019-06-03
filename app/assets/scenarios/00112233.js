6fddb79c9f1bb4586d896da0a5bcf494f857bd5b
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2019-06-03 10:01:54 +0100

    core service journey commit

---
+++ app/assets/scenarios/00112233.js
@@ -0,0 +1,15 @@
+module.exports = {
+  company: {
+    name: 'Spotlight LTD',
+    number: '00112233',
+    authCode: '00112233DEFSTATAC',
+    status: 'Active',
+    incorporationDate: '12 December 2008',
+    companyType: 'Private Limited Company',
+    address: '25 Bones Lane\nCardiff\nCF13 2WW',
+    accountsDue: '01 January 2020',
+    accountsStatus: 'notDue',
+    csDue: '15 May 2019',
+    csStatus: 'overdue'
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-
+router.get('/', function (req, res) {
+  res.render('start')
+})
+router.get('/start', function (req, res) {
+  res.render('start')
+})
+router.get('/sign-in', function (req, res) {
+  res.render('sign-in')
+})
+router.post('/sign-in', function (req, res) {
+  req.session.userEmail = req.body.email
+  res.redirect('company-number')
+})
+router.get('/company-number', function (req, res) {
+  res.render('company-number')
+})
+router.post('/company-number', function (req, res) {
+  var companyNumber = req.body.companyNumber
+  req.session.scenario = require('../assets/scenarios/' + companyNumber)
+  req.session.ptf = []
+  res.redirect('confirm-company')
+})
+router.get('/confirm-company', function (req, res) {
+  res.render('confirm-company', {
+    scenario: req.session.scenario
+  })
+})
 module.exports = router
