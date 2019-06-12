0c7a9f0d00ae95541bf1ca7026724b54295c954e
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2019-06-12 13:30:05 +0100

    Ask for more time to file

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Promise to file',
+  serviceName: 'Ask for more time to file',
 
   // Default port that prototype runs on
   port: '3800',
