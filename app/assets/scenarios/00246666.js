654690f669cce130af71e8bdfb595a6750058b25
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2019-06-17 15:05:47 +0100

    already requested ptf commit

---
+++ app/assets/scenarios/00246666.js
@@ -0,0 +1,17 @@
+module.exports = {
+  company: {
+    name: 'TWO MAN BAND LTD',
+    number: '00246666',
+    authCode: '00246666DEFSTATAA',
+    status: 'Active',
+    incorporationDate: '12 December 2008',
+    companyType: 'Private Limited Company',
+    address: '25 Bones Lane\nCardiff\nCF13 2WW',
+    accountsDue: '01 May 2019',
+    accountsStatus: 'overdue',
+    csDue: '15 January 2020',
+    csStatus: 'notDue',
+    reason: 'accounts',
+    PTFRequested: 'yes'
+  }
+}
