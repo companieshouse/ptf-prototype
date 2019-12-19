e92d1ddc82b767100760fa3f9c31ef525a1cebcb
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2019-12-19 16:34:00 +0000

    persistently late commit

--- app/routes.js
+++ app/routes.js
@@ -58,6 +58,7 @@ router.post('/how-to-authenticate', function (req, res) {
   var errorList = []
   var reference = req.body.reference
   var referenceErr = {}
+  var scenario = req.session.scenario
 
   switch (howToAuthenticate) {
     case 'authCode':
@@ -88,8 +89,12 @@ router.post('/how-to-authenticate', function (req, res) {
           reference: reference,
           scenario: req.session.scenario
         })
-        res.redirect('/continue-trading')
-        break
+        if (scenario.company.persistentlyLate === 'yes') {
+          res.redirect('/persistently-late')
+        } else {
+          res.redirect('/continue-trading')
+          break
+        }
       }
   }
 })
