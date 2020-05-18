1607f799e9c638c69fc74ddabdf81167d24b63a9
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2020-05-18 14:31:40 +0100

    version 5.2.1 commit
    LLP scenarios for CS only and CS and AA
    content consistency prosecution stop page
    content consistency no officers stop page
    content consistency company filed stop page
    content consistency prosecution stop page
    Re-uploaded missing persistently late screen
    content consistency persistently late screen
    Start screen content rework

---
+++ app/assets/scenarios/00112244.js
@@ -0,0 +1,20 @@
+module.exports = {
+  company: {
+    name: 'SINNLIG CANDLES LLP',
+    number: '00112244',
+    authCode: '444444',
+    status: 'Active',
+    incorporationDate: '12 December 2008',
+    companyType: 'Limited Liability Partnership',
+    address: '25 Bones Lane\nCardiff\nCF13 2WW',
+    accountsDue: '3 November 2019',
+    accountsStatus: 'notDue',
+    csDue: '15 March 2020',
+    csStatus: 'overdue',
+    reason: 'confirmation statement',
+    PTFRequested: 'no',
+    currentPenalty: '£150',
+    newDeadline: '25 February 2020',
+    prosecution: 'no'
+  }
+}
---
+++ app/assets/scenarios/00556677.js
@@ -0,0 +1,20 @@
+module.exports = {
+  company: {
+    name: 'MAGPIE PIES LLP',
+    number: '00556677',
+    authCode: '654321',
+    status: 'Active',
+    incorporationDate: '12 December 2008',
+    companyType: 'Limited Liability Partnership',
+    address: '25 Bones Lane\nCardiff\nCF13 2WW',
+    accountsDue: '3 November 2019',
+    accountsStatus: 'overdue',
+    csDue: '15 March 2020',
+    csStatus: 'overdue',
+    reason: 'accounts and confirmation statement',
+    PTFRequested: 'no',
+    currentPenalty: '£150',
+    newDeadline: '25 February 2020',
+    prosecution: 'no'
+  }
+}
