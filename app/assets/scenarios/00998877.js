4f6e7dced6786b50d8800e3711c84b40efb79c8f
Oceanne Esparcieux <oesparcieux@companieshouse.gov.uk>
2020-05-20 08:05:48 +0100

    5.2.2
    Updated scenario dates for testing
    Fixed content back to original on warning interruption page
    Changed content on prosecution stop page
    fixed company directors/ designanted members wording on 'co not required' screen

--- app/assets/scenarios/00998877.js
+++ app/assets/scenarios/00998877.js
@@ -7,13 +7,13 @@ module.exports = {
     incorporationDate: '12 December 2008',
     companyType: 'Private Limited Company',
     address: '25 Bones Lane\nCardiff\nCF13 2WW',
-    accountsDue: '3 November 2019',
+    accountsDue: '13 May 2020',
     accountsStatus: 'overdue',
-    csDue: '15 March 2020',
+    csDue: '15 July 2020',
     csStatus: 'notDue',
     reason: 'accounts',
     PTFRequested: 'no',
     currentPenalty: '£150',
-    newDeadline: '25 February 2020'
+    newDeadline: '7 June 2020'
   }
 }
